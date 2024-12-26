6039a1c81cb9733b81a7acab967638e2ca49a35d
jacobhsu <jacob.hsu.tw@gmail.com>
2024-12-26 18:38:28 +0800

    openai.com/v1/chat/completions

--- api/server.js
+++ api/server.js
@@ -1,7 +1,43 @@
-const express = require("express");
-const app = express();
+require("dotenv").config();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const API_KEY = process.env.API_KEY;
+
+app.get("/", (req, res) => res.send("Express on Vercel" + API_KEY));
+
+app.post("/completions", async (req, res) => {
+  // https://platform.openai.com/docs/api-reference/chat/create
+  // https://platform.openai.com/docs/models#model-endpoint-compatibility
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo", //gpt-4o-mini
+      messages: [{ role: "user", content: "Hi!" }],
+      max_tokens: 100,
+    }),
+  };
+
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
