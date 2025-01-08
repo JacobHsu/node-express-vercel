6b11a20aa9b82315ed2be0bf0d4bde43eec6c1bc
jacobhsu <jacob.hsu.tw@gmail.com>
2025-01-08 21:19:17 +0800

    fix: max_tokens

--- api/server.js
+++ api/server.js
@@ -49,7 +49,7 @@ app.post("/completions", async (req, res) => {
     body: JSON.stringify({
       model: "gpt-3.5-turbo", //gpt-4o-mini
       messages: [{ role: "user", content: req.body.message }],
-      max_tokens: 100,
+      max_tokens: 1000,
     }),
   };
 
